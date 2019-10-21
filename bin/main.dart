9a38f296ca96a7c6532a471fa1eccb429d30d047
McLarenCollege <hello@mclarencollege.in>
2019-10-21 07:17:24 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,25 @@
+// Challenge 1
+// Write failing test cases for next two challenges
+
+// Challenge 2
+// Write a function that sorts the positive numbers in ascending order,
+// and keeps the negative numbers untouched.
+// Example: posNegSort([6, 3, -2, 5, -8, 2, -2]) ➞ [2, 3, -2, 5, -8, 6, -2]
+
+// Challenge 3
+// The Karaca's Encryption Algorithm
+// Make a function that encrypts a given input with these steps:
+// Input: "apple"
+// Step 1: Reverse the input: "elppa"
+//
+//  Step 2: Replace all vowels using the following chart:
+// a => 0
+// e => 1
+// o => 2
+// u => 3
+// "1lpp0"
+// Step 3: Add "aca" to the end of the word: "1lpp0aca"
+// Output: "1lpp0aca"
+
+main() {
+}
